1f3fe191acc561bffcbb21872ebc38e96c430890
Dracon <petermarkcarey@gmail.com>
2021-09-05 17:22:10 +0800

    vrik rotation

--- RepositionBody.cs
+++ RepositionBody.cs
@@ -8,6 +8,7 @@ public class RepositionBody : MonoBehaviour
     [SerializeField] private Transform anchor;
     [SerializeField] private float down;
     [SerializeField] private float back;
+    [SerializeField] private bool rotate;
 
     // Start is called before the first frame update
     void Start()
@@ -19,5 +20,22 @@ public class RepositionBody : MonoBehaviour
     void Update()
     {
         transform.position = anchor.position + (Vector3.down * down) + (Vector3.back * back);
+        if (rotate)
+        {
+            Vector3 anchorDir = head.forward;
+            anchorDir.y = 0;
+            anchorDir.Normalize();
+
+            Vector3 rootDir = transform.forward;
+            rootDir.y = 0;
+            rootDir.Normalize();
+
+            Quaternion headAngle = Quaternion.LookRotation(anchorDir, Vector3.up);
+            Quaternion rootAngle = Quaternion.LookRotation(rootDir, Vector3.up);
+            if (Quaternion.Angle(headAngle, rootAngle) > 15)
+            {
+                transform.rotation = Quaternion.RotateTowards(rootAngle, headAngle, 90 * Time.deltaTime);
+            }
+        }
     }
 }
